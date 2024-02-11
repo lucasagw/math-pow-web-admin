2f91f3a44da3f881610df08d4f11d6cdec718e96
Cristiano Filho ☁️ <54041918+CristianoFIlho@users.noreply.github.com>
2024-02-11 13:02:05 -0300

    :sparkles: feat: create level types

---
+++ src/app/common/types/Level.ts
@@ -0,0 +1,7 @@
+export interface ILevel {
+  imageUrl: string;
+  title: string;
+  description: string;
+  startIn: number;
+  numberOfStart: number;
+}
---
+++ src/app/common/types/index.ts
@@ -0,0 +1,11 @@
+/*
+    IMPORTS
+*/
+// Types
+import { ILevel } from "./Level";
+
+/*
+    EXPORTS
+*/
+// Types
+export type { ILevel };
