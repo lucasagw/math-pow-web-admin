5c349bbe12c18120c7a5239d0f9577dbfd0047a9
Cristiano Filho ☁️ <54041918+CristianoFIlho@users.noreply.github.com>
2024-02-11 13:08:18 -0300

    :sparkles: feat: create level dtos

---
+++ src/app/[admin]/levels/dtos/levelDto.ts
@@ -0,0 +1,15 @@
+export interface CreateLevelDTO {
+  imageUrl: string;
+  title: string;
+  description: string;
+  startIn: number;
+  numberOfStart: number;
+}
+
+export interface UpdateLevelDto {
+  imageUrl: string;
+  title: string;
+  description: string;
+  startIn: number;
+  numberOfStart: number;
+}
