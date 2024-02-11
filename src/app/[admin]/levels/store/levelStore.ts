137fc55fe3ddd89f0e0adcc1d9921930af9ea175
Cristiano Filho ☁️ <54041918+CristianoFIlho@users.noreply.github.com>
2024-02-11 18:01:09 -0300

    :sparkles: feat: add form lib

--- src/app/[admin]/levels/store/levelStore.ts
+++ src/app/[admin]/levels/store/levelStore.ts
@@ -1,18 +1,22 @@
-import { revalidatePath } from "next/cache";
 import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
 // Repositories
 import levelRepo from "../repositories/levelRepo";
 // DTOS
 import { CreateLevelDTO } from "../dtos/levelDto";
 // Types
 import { ILevel } from "@/app/common/types";
+// Services
+import { storageService } from "@/app/common/services";
+// Schemas
+import { createLevelData } from "../schemas";
 
 interface LevelState {
   isLoading: boolean;
   levels: ILevel[];
   fetchAll: () => void;
   findByTitleOrDescription: (key: string) => void;
-  storeLevel: (createLevelDto: CreateLevelDTO) => void;
+  storeLevel: (createLevelDto: createLevelData) => void;
   deleteLevel: (uid: string) => void;
 }
 
@@ -28,18 +32,25 @@ export const useLevelStore = create<LevelState>()((_set) => ({
       console.error("Error when we tried to search", error);
     } finally {
       _set({ isLoading: false });
-      revalidatePath("/levels");
     }
   },
-  storeLevel: async (createLevelDto: CreateLevelDTO) => {
+  storeLevel: async (createLevelData: createLevelData) => {
     _set({ isLoading: true });
     try {
-      await levelRepo.createLevel(createLevelDto);
+      const fileList = createLevelData.imageUrl as FileList;
+      const remoteUrl = await storageService.uploadFile(
+        fileList[0],
+        `/levels/${fileList[0].name}${uuidv4()}`
+      );
+      const result: CreateLevelDTO = {
+        ...createLevelData,
+        imageUrl: remoteUrl,
+      };
+      await levelRepo.createLevel(result as CreateLevelDTO);
     } catch (error) {
       console.error("Error when we tried to store level", error);
     } finally {
       _set({ isLoading: false });
-      revalidatePath("/levels");
     }
   },
   deleteLevel: async (uid: string) => {
@@ -61,7 +72,6 @@ export const useLevelStore = create<LevelState>()((_set) => ({
       console.error("Error when we tried to fetch all levels", error);
     } finally {
       _set({ isLoading: false });
-      revalidatePath("/levels");
     }
   },
 }));
